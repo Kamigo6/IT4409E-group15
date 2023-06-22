0c3bc44125b721afcef6ccf60dfa97e3bdfca0ef
namtotbung <nam.nn265@gmail.com>
2023-06-22 17:13:22 +0700

    fix CORS problem

--- backend/config/express.js
+++ backend/config/express.js
@@ -8,6 +8,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
 app.use('/', routes);
 
 module.exports = app;
