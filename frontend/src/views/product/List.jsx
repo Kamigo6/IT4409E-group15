a14d530871cda0c584304237060f5de4a30cd782
namtotbung <nam.nn265@gmail.com>
2023-06-22 17:13:57 +0700

    calling api for products

--- frontend/src/views/product/List.jsx
+++ frontend/src/views/product/List.jsx
@@ -1,15 +1,13 @@
 import React, { lazy, Component } from "react";
-import { data } from "../../data";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTh, faBars } from "@fortawesome/free-solid-svg-icons";
+import axios from 'axios';
 const Paging = lazy(() => import("../../components/Paging"));
 const Breadcrumb = lazy(() => import("../../components/Breadcrumb"));
 const FilterCategory = lazy(() => import("../../components/filter/Category"));
 const FilterPrice = lazy(() => import("../../components/filter/Price"));
 const FilterSize = lazy(() => import("../../components/filter/Size"));
 const FilterStar = lazy(() => import("../../components/filter/Star"));
-const FilterColor = lazy(() => import("../../components/filter/Color"));
-const FilterTag = lazy(() => import("../../components/filter/Tag"));
 const FilterClear = lazy(() => import("../../components/filter/Clear"));
 const CardServices = lazy(() => import("../../components/card/CardServices"));
 const CardProductGrid = lazy(() =>
@@ -28,31 +26,31 @@ class ProductListView extends Component {
     view: "list",
   };
 
-  UNSAFE_componentWillMount() {
-    const totalItems = this.getProducts().length;
-    this.setState({ totalItems });
+  componentDidMount() {
+    this.getProducts();
   }
 
+  getProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/products");
+      const products = response.data;
+      const totalItems = products.length;
+      this.setState({ currentProducts: products, totalItems });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
   onPageChanged = (page) => {
-    let products = this.getProducts();
+    const { currentProducts } = this.state;
     const { currentPage, totalPages, pageLimit } = page;
     const offset = (currentPage - 1) * pageLimit;
-    const currentProducts = products.slice(offset, offset + pageLimit);
-    this.setState({ currentPage, currentProducts, totalPages });
+    const currentProductsPage = currentProducts.slice(offset, offset + pageLimit);
+    this.setState({ currentPage, currentProducts: currentProductsPage, totalPages });
   };
 
   onChangeView = (view) => {
     this.setState({ view });
-  };
-
-  getProducts = () => {
-    let products = data.products;
-    products = products.concat(products);
-    products = products.concat(products);
-    products = products.concat(products);
-    products = products.concat(products);
-    products = products.concat(products);
-    return products;
   };
 
   render() {
@@ -78,9 +76,7 @@ class ProductListView extends Component {
               <FilterPrice />
               <FilterSize />
               <FilterStar />
-              {/* <FilterColor /> */}
               <FilterClear />
-              {/* <FilterTag /> */}
               <CardServices />
             </div>
             <div className="col-md-9">
