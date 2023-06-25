ceffa842670600de4ac302cd1effa038d2698f41
namtotbung <nam.nn265@gmail.com>
2023-06-25 21:14:38 +0700

    CORS allow patch method

--- backend/config/express.js
+++ backend/config/express.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
   });
