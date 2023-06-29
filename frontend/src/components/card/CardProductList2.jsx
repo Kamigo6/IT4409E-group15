625b1fb9bee2b13476e887fd5da05d19bf7a6909
ngo_quang_minh <quangminhngo3@gmail.com>
2023-06-29 16:00:27 +0700

    update wishlist 1

--- frontend/src/components/card/CardProductList2.jsx
+++ frontend/src/components/card/CardProductList2.jsx
@@ -7,51 +7,33 @@ import { faCartPlus, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const CardProductList2 = (props) => {
   const product = props.data;
+  console.log(product);
   return (
     <div className="card">
       <div className="row g-0">
         <div className="col-md-3 text-center">
-          <img src={product.img} className="img-fluid" alt="..." />
+          <img src={product.imageUrls[0]} className="img-fluid" alt="..." />
         </div>
         <div className="col-md-9">
           <div className="card-body">
             <h6 className="card-subtitle me-2 d-inline">
-              <Link to={product.link} className="text-decoration-none">
+              <Link to={`/product/${product._id}`} className="text-decoration-none">
                 {product.name}
               </Link>
             </h6>
-            {product.isNew && (
-              <span className="badge bg-success me-2">New</span>
+            {product.isAvailable && (
+              <span className="badge bg-success me-2">Available</span>
             )}
-            {product.isHot && <span className="badge bg-danger me-2">Hot</span>}
-            {product.star > 0 && (
-              <span className="badge bg-secondary">
-                <IconStarFill className="text-warning i-va" width={12} height={12} /> {product.star}
-              </span>
-            )}
+            {!product.isAvailable && <span className="badge bg-danger me-2">Not Available</span>}
           </div>
 
           <div className="card-footer">
             <div className="mb-2">
-              <span className="fw-bold h5 me-2">${product.price}</span>
-              {product.originPrice > 0 && (
+              <span className="fw-bold h5 me-2">${product.price - product.discount.value}</span>
+              {product.discount.value > 0 && (
                 <del className="small text-muted me-2">
-                  ${product.originPrice}
+                  ${product.price}
                 </del>
-              )}
-              {(product.discountPercentage > 0 ||
-                product.discountPrice > 0) && (
-                <span className={`rounded p-1 bg-warning me-2 small`}>
-                  -
-                  {product.discountPercentage > 0
-                    ? product.discountPercentage + "%"
-                    : "$" + product.discountPrice}
-                </span>
-              )}
-              {product.isFreeShipping && (
-                <span className="text-success small mb-2">
-                  <IconTruckFill /> Free shipping
-                </span>
               )}
             </div>
 
--- frontend/src/views/account/Wishlist.jsx
+++ frontend/src/views/account/Wishlist.jsx
@@ -1,30 +1,77 @@
-import React, { Component, lazy } from "react";
-import { data } from "../../data";
+import React, { Component, lazy, useEffect, useState } from "react";
+import axios from 'axios';
+
 const CardProductList2 = lazy(() =>
   import("../../components/card/CardProductList2")
 );
 
-class WishlistView extends Component {
-  constructor(props) {
-    super();
-    this.state = {};
-  }
-  render() {
-    return (
-      <div className="container mb-3">
-        <h4 className="my-3">Wishlists</h4>
-        <div className="row g-3">
-          {data.products.map((product, idx) => {
-            return (
-              <div key={idx} className="col-md-6">
-                <CardProductList2 data={product} />
-              </div>
-            );
-          })}
-        </div>
+const WishlistView = () => {
+  const token = localStorage.getItem('token');
+  const [products, setProducts] = useState([]);
+  const [productsId, setProductsId] = useState([]);
+  const [customer, setCustomer] = useState(null);
+
+  useEffect(() => {
+    getCustomer();
+    // console.log(customer);
+    getProducts();
+    // console.log(products);
+  });
+
+  const getCustomer = async () => {
+    try {
+
+      const response = await fetch('http://localhost:8000/customers/token', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      if (response.ok) {
+        const customerData = await response.json();
+        setCustomer(customerData);
+
+        let array = [];
+        customerData.wishList.forEach(element => {
+          array.push(element.productId);
+        });
+        setProductsId(array);
+      } else {
+        console.error('Failed to fetch customer information');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  const getProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/products");
+
+      const products = response.data.filter((product) => {
+        return productsId.includes(product._id);
+      });
+      setProducts(products);
+
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  return (
+    <div className="container mb-3">
+      <h4 className="my-3">Wishlists</h4>
+      <div className="row g-3">
+        {products.map((product) => {
+          return (
+            <div className="col-md-6">
+              <CardProductList2 data={product} />
+            </div>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default WishlistView;
