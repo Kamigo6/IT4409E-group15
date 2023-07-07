100c18d918353d0716ea0895a17417593cbf926b
ngo_quang_minh <quangminhngo3@gmail.com>
2023-07-07 11:48:22 +0700

    update detail and rating

--- frontend/src/components/others/RatingsReviews.jsx
+++ frontend/src/components/others/RatingsReviews.jsx
@@ -7,11 +7,40 @@ import axios from 'axios';
 
 const RatingsReviews = ({ rating }) => {
   const [customer, setCustomer] = useState(null);
+  const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isDisliked, setIsDisliked] = useState(false);
 
+  const handleLikeClick = () => {
+    if (isLiked) {
+      setLikes(likes - 1);
+      setIsLiked(false);
+    } else if (!isDisliked) {
+      setLikes(likes + 1);
+      setIsLiked(true);
+    }
+  };
+
+  const handleDislikeClick = () => {
+    if (isDisliked) {
+      setDislikes(dislikes - 1);
+      setIsDisliked(false);
+    } else if (!isLiked) {
+      setDislikes(dislikes + 1);
+      setIsDisliked(true);
+    }
+  };
   useEffect(() => {
+    const token = localStorage.getItem('token');
+
     const getCustomer = async (id) => {
       try {
-        const response = await axios.get(`http://localhost:8000/customers/${id}`);
+        const response = await axios.get(`http://localhost:8000/customers/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
         const customer = response.data;
         setCustomer(customer);
       } catch (error) {
@@ -19,14 +48,16 @@ const RatingsReviews = ({ rating }) => {
       }
     };
 
-    // getCustomer(rating.customerId);
-
+    getCustomer(rating.customerId);
+    console.log(rating.customerId);
+    setLikes(rating.likes);
+    setDislikes(rating.dislikes)
   }, [])
+
   return (
     <div className="border-bottom mb-3">
       <div className="mb-2">
         <span>
-
           {Array.from({ length: rating.star }, (_, key) => (
             <IconStarFill className="text-warning me-1" key={key} />
           ))}
@@ -36,7 +67,7 @@ const RatingsReviews = ({ rating }) => {
         </span>
         <span className="text-muted">
           <IconCheckCircleFill className="text-success me-1" />
-          {customer && customer.name} | Reviewed on{" "}
+          {customer ? customer.username : "Undefined user"} | Reviewed on{" "}
           <i className="fw-bold">{new Date(rating.createdAt).toLocaleDateString()}</i>
         </span>
       </div>
@@ -44,11 +75,11 @@ const RatingsReviews = ({ rating }) => {
         {rating.content}
       </p>
       <div className="mb-2">
-        <button className="btn btn-sm btn-outline-success me-2">
-          <FontAwesomeIcon icon={faThumbsUp} /> {rating.likes}
+        <button className="btn btn-sm btn-outline-success me-2" onClick={handleLikeClick}>
+          <FontAwesomeIcon icon={faThumbsUp} /> {likes}
         </button>
-        <button className="btn btn-sm btn-outline-danger me-2">
-          <FontAwesomeIcon icon={faThumbsDown} /> {rating.dislikes}
+        <button className="btn btn-sm btn-outline-danger me-2" onClick={handleDislikeClick}>
+          <FontAwesomeIcon icon={faThumbsDown} /> {dislikes}
         </button>
       </div>
     </div>
